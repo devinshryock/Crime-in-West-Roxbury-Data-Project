3fb9b0faefec7b99840a3c5082194200903543f4
Devin Shryock <devinshryock@gmail.com>
2016-05-18 11:22:35 -0400

    Add Styling

--- West Roxbury Crime Data.R
+++ West Roxbury Crime Data.R
@@ -1,4 +1,5 @@
-#Script for City of Boston Crime Project
+#Author: Devin Shryock
+#City of Boston crime project to analyze crime in West Roxbury, MA
 #This data is from https://data.cityofboston.gov
 setwd("~/Desktop/LevelEDU /Labs/Week 4/Project#3")
 crime.data <- read.csv("Crime_Incident_Reports.csv")
@@ -10,11 +11,15 @@ west.roxbury.crime <- crime.data[which(crime.data$REPTDISTRICT == "E5"),]
 west.roxbury.crime <- west.roxbury.crime[which(west.roxbury.crime$Lat != 0),]
 
 #Plot crime latitudes using boxplot to identify outliers
-boxplot(west.roxbury.crime$Lat,main ="Latitude of Crime in West Roxbury",col="green")
+boxplot(west.roxbury.crime$Lat,
+        main ="Latitude of Crime in West Roxbury",
+        col="green")
 outliers.lat <- boxplot.stats(west.roxbury.crime$Lat)$out
 
 #Plot crime longitudes using boxplot to indentify outliers
-boxplot(west.roxbury.crime$Long,main = "Longitude of Crime in West Roxbury", col="red")
+boxplot(west.roxbury.crime$Long,
+        main = "Longitude of Crime in West Roxbury", 
+        col="red")
 outliers.long <- boxplot.stats(west.roxbury.crime$Long)$out
 
 #Plot all longitude and latitude crime points
@@ -28,7 +33,7 @@ west.roxbury.crime <- west.roxbury.crime[-c(1,2,3),]
 #Plot all data points again since outliers are now gone
 plot(west.roxbury.crime$Lat,west.roxbury.crime$Long,col="blue")
 
-##########################################################################################
+#############################################################################
 #Look at data using k-means clustering
 
 #Determine the number of clusters you should have (determine k)
@@ -37,8 +42,8 @@ for (i in 2:10){
   k.values[i] <- kmeans(west.roxbury.crime[22:23], centers = i)$tot.withinss
 }
 
-#Plot k-values to determine where the elbow "breaks".  This is the point where the 
-#error stops decreasing as much.  This is the optimal number of kclusters
+#Plot k-values to determine where the elbow "breaks".  This is the point where
+#the error stops decreasing as much.  This is the optimal number of kclusters
 plot(k.values)
 
 #Create 5 clusters of crime data points using kmeans clustering
